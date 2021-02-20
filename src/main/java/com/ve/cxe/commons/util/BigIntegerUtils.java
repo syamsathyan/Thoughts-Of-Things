51552a9b4cc0488a7bdbc4c815aaebefde6df759
Syam Sathyan <syam.sathyan@adp.com>
2021-02-20 01:38:01 -0500

    + BigIntegerUtils

---
+++ src/main/java/com/ve/cxe/commons/util/BigIntegerUtils.java
@@ -0,0 +1,19 @@
+package com.ve.cxe.commons.util;
+
+import java.math.BigInteger;
+
+public class BigIntegerUtils {
+
+    public static BigInteger concat(BigInteger x, BigInteger y) {
+        int ndigits = y.bitLength() * 3 / 10; // Guessed number of digits using 2^10 ≈ 10^3.
+        BigInteger pow10 = BigInteger.TEN.pow(ndigits);
+        while (pow10.compareTo(y) > 0) {
+            pow10 = pow10.divide(BigInteger.TEN);
+        }
+        while (pow10.compareTo(y) <= 0) {
+            pow10 = pow10.multiply(BigInteger.TEN);
+        }
+        // Cheating: int ndigits = y.toString().length();
+        return x.multiply(pow10).add(y);
+    }
+}
